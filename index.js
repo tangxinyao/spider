5d45b2394a4694da59557de513cffd6ac4fd80f7
tang <2578873659@qq.com>
2017-12-02 17:42:44 +0800

    remove some useless parts of headers

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const path = require('path');
 function fetchData() {
     request({
         url: 'http://nil.csail.mit.edu/6.824/2015/schedule.html',
-        headers: {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Host': 'nil.csail.mit.edu',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
-        },
         encoding: null
     }, function (err, res, body) {
         const $ = cheerio.load(body);
@@ -27,11 +21,7 @@ function fetchData() {
                 const filename = (temp ? temp[1] : href).split('?')[0];
                 const suffix = filename.split('.')[1];
                 if (suffix === 'pdf') {
-                    options.headers = {
-                        'Content-type': 'application/pdf',
-                        'Host': 'nil.csail.mit.edu',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-                    };
+                    options.headers = { 'Content-type': 'application/pdf' };
                 }
                 const url = /^http/.test(href) ? href : 'http://nil.csail.mit.edu/6.824/2015/' + href;
                 options.url = url;
