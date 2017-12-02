9fc3a08c373d8ac846b160c5602af72bc2676ee1
tang <2578873659@qq.com>
2017-12-02 17:27:52 +0800

    fix pdf download bug

--- index.js
+++ index.js
@@ -7,15 +7,10 @@ function fetchData() {
     request({
         url: 'http://nil.csail.mit.edu/6.824/2015/schedule.html',
         headers: {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Cache-Control": "max-age=0",
-            "Host": "nil.csail.mit.edu",
-            "Proxy-Connection": "keep-alive",
-            "Referer": "http://nil.csail.mit.edu/6.824/2015/general.html",
-            "Upgrade-Insecure-Requests": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36"
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Host': 'nil.csail.mit.edu',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
         },
         encoding: null
     }, function (err, res, body) {
@@ -26,21 +21,29 @@ function fetchData() {
                 fs.mkdirSync(dirname);
             }
             $(lecture).find('a').each((j, datum) => {
+                const options = {}
                 const href = $(datum).attr('href');
                 const temp = /.*\/(.*)$/.exec(href);
                 const filename = (temp ? temp[1] : href).split('?')[0];
-                const uri = /^http/.test(href) ? href : 'http://nil.csail.mit.edu/6.824/2015/' + href;
-                request(uri, function (err, res, body) {
+                const suffix = filename.split('.')[1];
+                if (suffix === 'pdf') {
+                    options.headers = {
+                        'Content-type': 'application/pdf',
+                        'Host': 'nil.csail.mit.edu',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+                    };
+                }
+                const url = /^http/.test(href) ? href : 'http://nil.csail.mit.edu/6.824/2015/' + href;
+                options.url = url;
+                options.encoding = null;
+                request(options, function (err, res, body) {
                     if (err || res.statusCode !== 200) {
-                        console.log(`error: fetch ${dirname} with ${uri}`);
+                        console.log(`error: fetch ${dirname} with ${url}`);
                         return;
-                    }
-                    if (res.headers &&res.headers['content-type'] === 'application/pdf') {
-                        // TODO: handle the pdf.
                     }
                     fs.writeFile(path.resolve(dirname, filename), body, function (err) {
                         if (err) {
-                            console.log(dirname + ' with ' + uri);
+                            console.log(dirname + ' with ' + url);
                         }
                     });
                 });
